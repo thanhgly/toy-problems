637ffde0feb705b4e9335ef26c0492695c2d2c9b
Thanh Ly <thanhgly.se@gmail.com>
2023-06-05 18:40:04 -0700

    Completed two-sum in TypeScript

---
+++ 1.two-sum.ts
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=1 lang=typescript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[] {
+  let visited: object = {};
+  for (let i: number = 0; i < nums.length; i++) {
+    const disiredNum = target - nums[i];
+    if (visited[disiredNum] !== undefined) {
+      return [visited[disiredNum], i];
+    }
+    visited[nums[i]] = i;
+  }
+  return [];
+};
+// @lc code=end
+
