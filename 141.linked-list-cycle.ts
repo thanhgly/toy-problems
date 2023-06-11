91ff13e78e7f76c080ab5bca0b04f929d2f444c1
Thanh Ly <thanhgly.se@gmail.com>
2023-06-10 19:05:45 -0700

    Completed linked-list-cycle in TypeScript

---
+++ 141.linked-list-cycle.ts
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=141 lang=typescript
+ *
+ * [141] Linked List Cycle
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function hasCycle(head: ListNode | null): boolean {
+  if (head === null) {
+    return false;
+  }
+  let fast = head.next;
+  let slow = head;
+  while (fast !== null && fast.next !== null) {
+    if (fast === slow) {
+      return true;
+    }
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+  return false;
+};
+// @lc code=end
+
+// O: boolean
+// I: ListNode
+// C: NA (always verify with interviewer)
+// E: list with length 0, 1 should return false
+
+// pseudocode
+// fast and slow pointer technique
+// if head == null return false
+// declare fast = head.next
+// declare slow = head
+// while fast != null
+//   if fast == slow
+//     return true
+//   fast = fast.next.next
+//   slow = slow.next
+// return false
