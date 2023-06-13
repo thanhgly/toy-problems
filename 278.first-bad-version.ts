0bdf454b1e7bd30239c3af6726f5bcf660bf1ad0
Thanh Ly <thanhgly.se@gmail.com>
2023-06-12 17:27:56 -0700

    Completed first-bad-version in TypeScript (binary search aproach)

---
+++ 278.first-bad-version.ts
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=278 lang=typescript
+ *
+ * [278] First Bad Version
+ */
+
+// @lc code=start
+/**
+ * The knows API is defined in the parent class Relation.
+ * isBadVersion(version: number): boolean {
+ *     ...
+ * };
+ */
+
+var solution = function(isBadVersion: any) {
+
+    return function(n: number): number {
+        let l = 1;
+        let r = n;
+        while (l < r) {
+            let mid = Math.floor((l + r) / 2);
+            if (isBadVersion(mid)) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    };
+};
+// @lc code=end
+
