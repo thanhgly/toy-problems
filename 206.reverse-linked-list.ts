da59ed2118877ce87a9a0a0c6a6b12bee97b5107
Thanh Ly <thanhgly.se@gmail.com>
2023-06-14 23:26:31 -0700

    Completed reverse-linked-list in TypeScript (recursive approach)

--- 206.reverse-linked-list.ts
+++ 206.reverse-linked-list.ts
@@ -16,16 +16,31 @@
  *     }
  * }
  */
+// Iteration Version
+// function reverseList(head: ListNode | null): ListNode | null {
+//   let newHead = null;
+//   let curNode = head;
+//   while (curNode !== null) {
+//     let tail = newHead;
+//     newHead = new ListNode(curNode.val, tail);
+//     curNode = curNode.next;
+//   }
+//   return newHead;
+// };
 
+// Recursive version
 function reverseList(head: ListNode | null): ListNode | null {
-  let newHead = null;
-  let curNode = head;
-  while (curNode !== null) {
-    let tail = newHead;
-    newHead = new ListNode(curNode.val, tail);
-    curNode = curNode.next;
-  }
-  return newHead;
+  function reverse(node: ListNode | null, prev: ListNode | null): ListNode | null {
+    if (node === null) {
+      return prev;
+    }
+
+    const next = node.next;
+    node.next = prev;
+    return reverse(next, node);
+  };
+
+  return reverse(head, null);
 };
 // @lc code=end
 
@@ -46,4 +61,10 @@ function reverseList(head: ListNode | null): ListNode | null {
  *   newHead = new ListNode(curNode.val, tail)
  *   curNode = curNode.next
  * return newHead
+ */
+/**
+ * Recursive version
+ * if (head.next === null)
+ *   return head
+ * return new ListNode(head.val, reverseList(head.next))
  */
